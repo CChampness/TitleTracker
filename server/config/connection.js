87c81484da29899d060f7c6cc9977966f6d63f61
CChampness <87551272+CChampness@users.noreply.github.com>
2022-02-03 10:56:59 -0500

    fixing URI for heroku mongo

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://cchampness:SeaSprite&1977@yourCluster0.n9z04.mongodb.net/sample_mflix?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
