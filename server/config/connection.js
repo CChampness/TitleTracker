db60caa8028195d64b6ad20e42d2982f001015aa
CChampness <87551272+CChampness@users.noreply.github.com>
2022-02-03 14:33:17 -0500

    heroku deploy

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://cchampness:SeaSprite&1977@Cluster0.n9z04.mongodb.net/titletracker?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://cchampness:SeaSprite&1977@Cluster0.n9z04.mongodb.net/shrouded-meadow-26197r?retryWrites=true&w=majority', {
 // mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
