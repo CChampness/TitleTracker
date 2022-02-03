129e64cc3bf88a4c707725c15e78a11a64bb1b6c
CChampness <87551272+CChampness@users.noreply.github.com>
2022-02-03 15:04:33 -0500

    heroku deploy

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://cchampness:SeaSprite&1977@Cluster0.n9z04.mongodb.net/shrouded-meadow-26197r?retryWrites=true&w=majority', {
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://cchampness:SeaSprite&1977@Cluster0.n9z04.mongodb.net/shrouded-meadow-26197r?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
